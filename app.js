d03680bc3f97a8f567feef001522641682b81587
HMantas <mantas.herkus.lileikis@gmail.com>
2022-11-21 13:21:26 +0200

    Add burger menu x animation

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 
-let element = document.querySelector(".hamburger");
+let burgerMenu = document.querySelector(".hamburger");
+let menuLink = document.querySelectorAll(".nav-link");
 
-element.addEventListener("click", () => {
+burgerMenu.addEventListener("click", () => {
   document.querySelector(".nav-links").classList.toggle("active");
   document.querySelector(".navigation").classList.toggle("fixed");
-  document.querySelector(".hero").classList.toggle("margin");
+  document.querySelector(".hero").classList.toggle("margin-80");
+  document.querySelector(".hamburger").classList.toggle("burger-active");
 });
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  document.querySelector(".nav-links").classList.remove("active");
+  document.querySelector(".hamburger").classList.remove("burger-active");
+}));
+
+// document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+//   document.querySelector(".hamburger").classList.add("burger-active");
+// }));
+
