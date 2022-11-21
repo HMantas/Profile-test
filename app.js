9223400b1008127aaa4031fd296af0cdb1e02da1
HMantas <mantas.herkus.lileikis@gmail.com>
2022-11-21 12:03:30 +0200

    Add hamburger menu

---
+++ app.js
@@ -0,0 +1,7 @@
+
+let element = document.querySelector(".hamburger");
+
+element.addEventListener("click", () => {
+  document.querySelector(".nav-links").classList.toggle("active");
+  document.querySelector(".navigation").classList.toggle("fixed");
+});
