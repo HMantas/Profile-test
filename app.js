4d173ed991ac7c48c8d4b9b8cbb45f37ebb59a17
HMantas <mantas.herkus.lileikis@gmail.com>
2022-11-21 12:22:54 +0200

    Fix burger menu jump problem with margin

--- app.js
+++ app.js
@@ -4,4 +4,5 @@ let element = document.querySelector(".hamburger");
 element.addEventListener("click", () => {
   document.querySelector(".nav-links").classList.toggle("active");
   document.querySelector(".navigation").classList.toggle("fixed");
+  document.querySelector(".hero").classList.toggle("margin");
 });
