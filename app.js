7b2c18cff3936e4893bedfbdd249618adc82c422
HMantas <mantas.herkus.lileikis@gmail.com>
2022-11-24 13:09:57 +0200

    Make burger menu fullscreen on mobile

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 
 let burgerMenu = document.querySelector(".hamburger");
 let menuLink = document.querySelectorAll(".nav-link");
+let noDisplay = document.querySelector(".hero");
 
 burgerMenu.addEventListener("click", () => {
   document.querySelector(".nav-links").classList.toggle("active");
-  document.querySelector(".navigation").classList.toggle("fixed");
-  document.querySelector(".hero").classList.toggle("margin-80");
+  document.querySelector(".about").classList.toggle("hidden");
+  noDisplay.classList.toggle("hidden");
+  document.querySelector(".footer").classList.toggle("hidden");
   document.querySelector(".hamburger").classList.toggle("burger-active");
 });
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
   document.querySelector(".nav-links").classList.remove("active");
   document.querySelector(".hamburger").classList.remove("burger-active");
+  document.querySelector(".about").classList.remove("hidden");
+  document.querySelector(".hero").classList.remove("hidden");
+  document.querySelector(".footer").classList.remove("hidden");
 }));
 
 // document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
 //   document.querySelector(".hamburger").classList.add("burger-active");
 // }));
+
+
+
 
