0f49eb8262937e890bc597b3b2eb11945e5b63a3
HMantas <mantas.herkus.lileikis@gmail.com>
2023-01-27 21:50:53 +0200

    Add about section

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 
+//Burger menu
+
 let burgerMenu = document.querySelector(".hamburger");
 let menuLink = document.querySelectorAll(".nav-link");
-let noDisplay = document.querySelector(".hero");
 let menuLinks = document.querySelector(".nav-links");
-let about = document.querySelector(".about");
 let footer = document.querySelector(".footer");
+let main = document.querySelector(".main")
 
 burgerMenu.addEventListener("click", () => {
   menuLinks.classList.toggle("active");
-  about.classList.toggle("hidden");
-  noDisplay.classList.toggle("hidden");
   footer.classList.toggle("hidden");
   burgerMenu.classList.toggle("burger-active");
+  main.classList.toggle("hidden");
 });
 
 menuLink.forEach(n => n.addEventListener("click", () => {
   menuLinks.classList.remove("active");
   burgerMenu.classList.remove("burger-active");
-  about.classList.remove("hidden");
-  noDisplay.classList.remove("hidden");
+  main.classList.remove("hidden");
   footer.classList.remove("hidden");
 }));
 
