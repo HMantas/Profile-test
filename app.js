4b1a7df40e058cb3b22e413891c30dd9941a38e9
HMantas <mantas.herkus.lileikis@gmail.com>
2023-01-28 16:17:46 +0200

    Testing color themes

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ let main = document.querySelector(".main")
 
 burgerMenu.addEventListener("click", () => {
   menuLinks.classList.toggle("active");
-  footer.classList.toggle("hidden");
   burgerMenu.classList.toggle("burger-active");
+  footer.classList.toggle("hidden");
   main.classList.toggle("hidden");
 });
 
@@ -25,16 +25,29 @@ menuLink.forEach(n => n.addEventListener("click", () => {
 let colorChanger = document.querySelector(".color-changer");
 // Font color
 let navigation = document.querySelector(".navigation");
+let creditsText = document.querySelector(".credits__text");
 
 // Background color
-let bar = document.querySelector(".bar");
-let button = document.querySelector(".btn")
+let bar = document.querySelectorAll(".bar");
+let button = document.querySelectorAll(".btn");
+let box = document.querySelectorAll(".box");
 
 colorChanger.addEventListener("click", () => {
+  for (let i = 0; i < box.length; i++) {
+    box[i].style.backgroundColor = "#facf0f";
+  };
+  for (let i = 0; i < button.length; i++) {
+    button[i].style.backgroundColor = "#facf0f";
+  };
+  for (let i = 0; i < bar.length; i++) {
+    bar[i].style.backgroundColor = "#facf0f";
+  };
   navigation.classList.toggle("yellow-font");
-  bar.classList.toggle("yellow-background");
-  button.classList.toggle("yellow-background");
-})
+  creditsText.classList.toggle("yellow-font");
+  console.log(button);
+});
+
+
 
 
 
