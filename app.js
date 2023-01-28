64a71122f4b1b6ff9b3157cb916ddfa656b19e44
HMantas <mantas.herkus.lileikis@gmail.com>
2023-01-28 17:25:56 +0200

    Color testing

--- app.js
+++ app.js
@@ -30,26 +30,49 @@ let creditsText = document.querySelector(".credits__text");
 // Background color
 let bar = document.querySelectorAll(".bar");
 let button = document.querySelectorAll(".btn");
+let navLinksText = document.querySelectorAll(".nav-link-text");
 let box = document.querySelectorAll(".box");
+let fade = document.querySelectorAll(".background__fade");
 
 colorChanger.addEventListener("click", () => {
+  for (let i = 0; i < fade.length; i++) {
+    fade[i].classList.toggle("yellow-background");
+    fade[i].classList.add("transition");
+  };
   for (let i = 0; i < box.length; i++) {
     box[i].classList.toggle("yellow-background");
     box[i].classList.add("transition");
   };
+
   for (let i = 0; i < button.length; i++) {
     button[i].classList.toggle("yellow-background");
     button[i].classList.add("transition");
   };
+
+  for (let i = 0; i < navLinksText.length; i++) {
+    navLinksText[i].classList.toggle("yellow-font");
+    navLinksText[i].classList.add("transition");
+  };
+
   for (let i = 0; i < bar.length; i++) {
     bar[i].classList.toggle("yellow-background");
   };
+
   navigation.classList.toggle("yellow-font");
   navigation.classList.add("transition");
   creditsText.classList.toggle("yellow-font");
   creditsText.classList.add("transition");
+  colorChanger.classList.toggle("blur");
+
+  if(colorChanger.innerHTML === "Blue") {
+    colorChanger.innerHTML = "Yellow";
+  } else {
+    colorChanger.innerHTML = "Blue";
+  }
 });
 
+
+console.log(colorChanger.innerHTML)
 
 
 
